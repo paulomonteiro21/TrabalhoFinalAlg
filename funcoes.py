4d3405a4c07c5fb0340a2b31c2e3bf5748ab62e5
Paulo <paulo.monteiro@estudante.uffs.edu.br>
2026-06-17 15:28:55 -0300

    função alterar backup

--- funcoes.py
+++ funcoes.py
@@ -24,6 +24,7 @@ def cadastrar_livro(livros):
     livro.status = 'Disponível'
     livros.append(livro)
     return livros
+
 def consultar_livros(livros):
     opc = int(input('Digite 1 para consultar por código ou 2 para consultar por autor: '))
     if opc == 1:
@@ -38,37 +39,41 @@ def consultar_livros(livros):
         for i in range(len(livros)):
             if autor == livros[i].autor:
                print_livros(livros,i)
+
 def print_livros(livros,i):
         '''print(f'Título: {livros[i].titulo}')'''
         print(f'Autor: {livros[i].autor}')
         '''print(f'Ano: {livros[i].Ano}')'''
         print(f'Código: {livros[i].codigo}')
         '''print(f'Status: {livros[i].status}')''' 
-def alterar_livro(livros,i):
+
+def alterar_livro(livros):
      codigo = input('Digite o Código do livro que você deseja alterar: ')
-     pega_codigo(livros,codigo)
-     if i!=1:
-        print(f'')
-        livro = Livros()
-        '''livro.titulo = input('Digite o título do livro: ')'''
-        livro.autor = input('Digite o autor do livro: ')
-        '''livro.Ano = int(input('Digite o ano de publicação do livro: '))'''
-        livro.codigo = input('Digite o código do livro: ')
-     
-     
+     i = pega_codigo(livros,codigo)
+     if i!=-1:
+        print(f'Digite os NOVOS DADOS:')
+        '''livro[i].titulo = input('Digite o título do livro: ')'''
+        livros[i].autor = input('Digite o autor do livro: ')
+        '''livro[i].Ano = int(input('Digite o ano de publicação do livro: '))'''
+        livros[i].codigo = input('Digite o código do livro: ')
+        print(f'Livro alterado com sucesso!')
+     else:
+          print(f'Livro não encontrado')
+
 def main():
     op = 0
     livros = []
     while True:
-        print('1 - Cadastrar livro')
-        print('2 - Consultar livros')
-        print('0 - Sair')
-        op = int(input('Digite a opção desejada: '))
+        print(f'1 - Cadastrar livro')
+        print(f'2 - Consultar livros')
+        print(f'3 - Alterar livros')
+        print(f'0 - Sair')
+        op = int(input(f'Digite a opção desejada: '))
         if op == 1:
             cadastrar_livro(livros)
         if op == 2:
             consultar_livros(livros)
-        if op == 0:
+        if op == 3:
              alterar_livro(livros)
         if op == 0:
             break
