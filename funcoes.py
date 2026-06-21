da385bd7f5ccbc922f097c926b2bc475ef430495
Paulo <paulo.monteiro@estudante.uffs.edu.br>
2026-06-21 17:39:02 -0300

    finalizando o trabalho, fazendo a parte de emprestar e devolver

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,7 @@
 class Livros:
     titulo = ''
     autor = ''
-    Ano = 0
+    ano = 0
     codigo = ''
     status = ''
 
@@ -13,9 +13,9 @@ def pega_codigo(livros,codigo):
 
 def cadastrar_livro(livros):
     livro = Livros()
-    '''livro.titulo = input('Digite o título do livro: ')'''
+    livro.titulo = input('Digite o título do livro: ')
     livro.autor = input('Digite o autor do livro: ')
-    '''livro.Ano = int(input('Digite o ano de publicação do livro: '))'''
+    livro.ano = int(input('Digite o ano de publicação do livro: '))
     livro.codigo = input('Digite o código do livro: ')
     i = pega_codigo(livros,livro.codigo)
     if i!=-1:
@@ -41,11 +41,11 @@ def consultar_livros(livros):
                print_livros(livros,i)
 
 def print_livros(livros,i):
-        '''print(f'Título: {livros[i].titulo}')'''
+        print(f'Título: {livros[i].titulo}')
         print(f'Autor: {livros[i].autor}')
-        '''print(f'Ano: {livros[i].Ano}')'''
+        print(f'ano: {livros[i].ano}')
         print(f'Código: {livros[i].codigo}')
-        '''print(f'Status: {livros[i].status}')''' 
+        print(f'Status: {livros[i].status}')
 
 def alterar_livro(livros):
      codigo = input('Digite o Código do livro que você deseja alterar: ')
@@ -54,19 +54,45 @@ def alterar_livro(livros):
         print(f'Digite os NOVOS DADOS:')
         '''livro[i].titulo = input('Digite o título do livro: ')'''
         livros[i].autor = input('Digite o autor do livro: ')
-        '''livro[i].Ano = int(input('Digite o ano de publicação do livro: '))'''
+        '''livro[i].ano = int(input('Digite o ano de publicação do livro: '))'''
         livros[i].codigo = input('Digite o código do livro: ')
         print(f'Livro alterado com sucesso!')
      else:
           print(f'Livro não encontrado')
 
+def remover_livro(livros):
+     codigo = input('Digite o código do livro a ser removido: ')
+     i=pega_codigo(livros,codigo)
+     if i !=-1:
+          livros.pop(i)
+     else:
+         print(f'Livro não encontrado')
+
+def listar_livros(livros,livros_emprestados):
+     for i in range(len(livros)):  
+        print(f'{livros[i].titulo}, {livros[i].ano}')
+
+def emprestar_livro(livros):
+     codigo = input('Digite o código do livro a ser emprestado: ')
+     i=pega_codigo(livros,codigo)
+     if i!=-1:
+          livros_emprestados.append(livros[i].codigo)
+          
+    i2=pega_codigo(livros_emprestados,codigo)
+        if i2!=-1:
+            print('Livro já emprestado')
+
+def devolver_livros(livros):
 def main():
     op = 0
     livros = []
+    livros_emprestados = []
     while True:
         print(f'1 - Cadastrar livro')
         print(f'2 - Consultar livros')
         print(f'3 - Alterar livros')
+        print(f'4 - Remover livros')
+        print(f'5 - Listar livros')
         print(f'0 - Sair')
         op = int(input(f'Digite a opção desejada: '))
         if op == 1:
@@ -75,6 +101,14 @@ def main():
             consultar_livros(livros)
         if op == 3:
              alterar_livro(livros)
+        if op == 4:
+             remover_livro(livros)
+        if op == 5:
+             listar_livros(livros)
+        if op == 6:
+             emprestar_livro(livros)
+        if op == 7:
+             devolver_livro(livros)
         if op == 0:
             break
 
